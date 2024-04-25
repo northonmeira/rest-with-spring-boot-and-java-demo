b8cef787e8d823c3f5c00648bed55246d71c4c96
northon morais <northonmm@gmail.com>
2024-04-25 16:58:48 -0300

    first commit

---
+++ src/main/java/br/com/northon/demo/Greeting.java
@@ -0,0 +1,30 @@
+package br.com.northon.demo;
+
+public class Greeting {
+	
+	private int id;
+	private String name;
+	
+	public Greeting(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
---
+++ src/main/java/br/com/northon/demo/RestWithSpringBootAndJavaDemoApplication.java
@@ -0,0 +1,13 @@
+package br.com.northon.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSpringBootAndJavaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithSpringBootAndJavaDemoApplication.class, args);
+	}
+
+}
