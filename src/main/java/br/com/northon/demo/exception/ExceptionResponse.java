e00a505623c7445b260782dae59df6eeccf9324f
northon morais <northonmm@gmail.com>
2024-04-25 17:30:32 -0300

    Tratamento de excecao

---
+++ src/main/java/br/com/northon/demo/exception/ExceptionResponse.java
@@ -0,0 +1,46 @@
+package br.com.northon.demo.exception;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class ExceptionResponse implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private Date timestamp;
+	private String message;
+	private String Details;
+	
+	
+	
+	public ExceptionResponse(Date timestamp, String message, String details) {
+		super();
+		this.timestamp = timestamp;
+		this.message = message;
+		Details = details;
+	}
+	
+	public Date getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(Date timestamp) {
+		this.timestamp = timestamp;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getDetails() {
+		return Details;
+	}
+	public void setDetails(String details) {
+		Details = details;
+	}
+	
+
+}
