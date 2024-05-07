2bfac5074f2ba2fd5def4be051c50d82f9cd2e52
northon morais <northonmm@gmail.com>
2024-05-07 19:26:05 -0300

    adicionando CORS validation and integration tests

--- src/main/java/br/com/northon/demo/controller/PersonController.java
+++ src/main/java/br/com/northon/demo/controller/PersonController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,6 +32,7 @@ public class PersonController {
 	@Autowired
 	private PersonService personService;
 
+	@CrossOrigin(origins = {"http://localhost:8080"})
 	@GetMapping(value= "/{id}", produces = {MediaType.APPLICATION_JSON, 
 			MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
 	@Operation(summary = "Finds a Person", description = "Finds a Person",
